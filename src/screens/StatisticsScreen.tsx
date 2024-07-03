55aadcf861d8be853a41c15314ccd25e7aa70175
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:42:39 +0300

    updated statistics screen

---
+++ src/screens/StatisticsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const StatisticsScreen = () => (
+  <View>
+    <Text>Statistics Screen</Text>
+  </View>
+);
+
+export default StatisticsScreen;
