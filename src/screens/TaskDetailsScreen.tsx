adf8c0c4a25379469eb96be579a0055b427c3e2a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:39:21 +0300

    updated taskDetails screen

---
+++ src/screens/TaskDetailsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const TaskDetailsScreen = () => (
+  <View>
+    <Text>Task Details Screen</Text>
+  </View>
+);
+
+export default TaskDetailsScreen;
