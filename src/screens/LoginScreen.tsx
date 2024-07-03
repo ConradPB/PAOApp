7a8dbf10fb3433449200a6cb73432b9b1712cb8c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:31:00 +0300

    updated Login

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const LoginScreen = () => (
+  <View>
+    <Text>Login Screen</Text>
+  </View>
+);
+
+export default LoginScreen;
