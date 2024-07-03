09744538b1e05403f14d8f4cbc74a949972bc77e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:36:45 +0300

    updated task list

---
+++ src/screens/TaskListScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const TaskListScreen = () => (
+  <View>
+    <Text>Task List Screen</Text>
+  </View>
+);
+
+export default TaskListScreen;
