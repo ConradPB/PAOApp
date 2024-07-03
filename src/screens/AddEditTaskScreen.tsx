ac6f00f9da06520e614c44c6af495ac8728f343e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:41:11 +0300

    updated addedit task

---
+++ src/screens/AddEditTaskScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const AddEditTaskScreen = () => (
+  <View>
+    <Text>Add/Edit Task Screen</Text>
+  </View>
+);
+
+export default AddEditTaskScreen;
