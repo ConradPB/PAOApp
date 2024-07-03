320092572c742c1b6ec20b9fbf1f646c3e69b5b1
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:34:14 +0300

    updated home and profile

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => (
+  <View>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+export default HomeScreen;
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const ProfileScreen = () => (
+  <View>
+    <Text>Profile Screen</Text>
+  </View>
+);
+
+export default ProfileScreen;
