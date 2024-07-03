bd02d05c590c395b30d5a9e13e02066ce4deae69
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:35:48 +0300

    updated settings screen

---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const SettingsScreen = () => (
+  <View>
+    <Text>Settings Screen</Text>
+  </View>
+);
+
+export default SettingsScreen;
