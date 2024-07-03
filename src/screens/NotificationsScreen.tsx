8244ec7358348ed00e7475f51cf6ea0020af4eb2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-03 05:46:51 +0300

    updared notifications

---
+++ src/screens/NotificationsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
+
+const NotificationsScreen = () => (
+  <View>
+    <Text>Notifications Screen</Text>
+  </View>
+);
+
+export default NotificationsScreen;
