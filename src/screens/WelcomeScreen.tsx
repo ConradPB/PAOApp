8a34f9ae916e51b463972fa59a18dc6eac823940
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-06 02:29:24 +0300

    updated Welcome screen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
 import { View, Text, Button } from 'react-native';
-
 // eslint-disable-next-line prettier/prettier
-const WelcomeScreen = ({ navigation }) => (
-  <View>
-    <Text>Welcome Screen</Text>
-    <Button title="Login" onPress={() => navigation.navigate('Login')} />
-    <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
-  </View>
-);
+import { useNavigation } from '@react-navigation/native';
+
+const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text>Welcome to PAO - Break Bread</Text>
+      <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
+      <Button title="Login" onPress={() => navigation.navigate('Login')} />
+    </View>
+  );
+};
 
 export default WelcomeScreen;
+
