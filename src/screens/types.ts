15b0cbae88ce2b4dc9b233931675c0027b3f2867
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-06 03:38:35 +0300

    added types

---
+++ src/screens/types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export type RootStackParamList = {
+    Welcome: undefined;
+    SignUp: undefined;
+    Login: undefined;
+    Home: undefined;
+    TaskList: undefined;
+    TaskDetails: { id: string };
+    AddEditTask: { id?: string };
+  };
+  
