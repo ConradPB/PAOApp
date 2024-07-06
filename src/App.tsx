f90f594969f94de220fc7e49fec7f40a7a97e605
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-06 03:52:11 +0300

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-// Import screen components
+// eslint-disable-next-line prettier/prettier
+import { NavigationContainer } from '@react-navigation/native';
+// eslint-disable-next-line prettier/prettier
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+// eslint-disable-next-line prettier/prettier
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// eslint-disable-next-line prettier/prettier
+import { RouteProp } from '@react-navigation/native';
 import WelcomeScreen from './screens/WelcomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -14,10 +17,11 @@ import SettingsScreen from './screens/SettingsScreen';
 import TaskListScreen from './screens/TaskListScreen';
 import TaskDetailsScreen from './screens/TaskDetailsScreen';
 import AddEditTaskScreen from './screens/AddEditTaskScreen';
-import StatisticsScreen from './screens/StatisticsScreen';
-import NotificationsScreen from './screens/NotificationsScreen';
 
-const Stack = createStackNavigator();
+// eslint-disable-next-line prettier/prettier
+import { RootStackParamList } from './types';
+
+const Stack = createStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator();
 
 const HomeTabNavigator = () => (
@@ -25,7 +29,6 @@ const HomeTabNavigator = () => (
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Profile" component={ProfileScreen} />
     <Tab.Screen name="Settings" component={SettingsScreen} />
-    <Tab.Screen name="Notifications" component={NotificationsScreen} />
   </Tab.Navigator>
 );
 
@@ -39,7 +42,6 @@ const App = () => (
       <Stack.Screen name="TaskList" component={TaskListScreen} />
       <Stack.Screen name="TaskDetails" component={TaskDetailsScreen} />
       <Stack.Screen name="AddEditTask" component={AddEditTaskScreen} />
-      <Stack.Screen name="Statistics" component={StatisticsScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
