2ea6ac4884aa5e3af73ea2c858b31dc82ff41e25
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 00:04:30 +0300

    updated Login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import { useNavigation, StackNavigationProp } from '@react-navigation/native';
