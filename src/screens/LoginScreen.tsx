8c9b5608d54a37746599a4933c5070504ea529b0
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-06 23:59:21 +0300

    updated

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-// eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useNavigation, StackNavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../types';
 
-const LoginScreen = () => (
-  <View>
-    <Text>Login Screen</Text>
-  </View>
-);
+type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation<LoginScreenNavigationProp>();
+
+  const handleLogin = () => {
+    // Handle login logic here
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View>
+      <Text>Login</Text>
+      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
 
 export default LoginScreen;
