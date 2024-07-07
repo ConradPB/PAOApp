6a7a2dd1580f5f90ce07630cc07fbcad104e1409
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 12:38:12 +0300

    added sign up config

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-// eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useNavigation, StackNavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../types';
 
-const SignUpScreen = () => (
-  <View>
-    <Text>Sign Up Screen</Text>
-  </View>
-);
+type SignUpScreenNavigationProp = StackNavigationProp<RootStackParamList, 'SignUp'>;
+
+const SignUpScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation<SignUpScreenNavigationProp>();
+
+  const handleSignUp = () => {
+    // Handle sign up logic here
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View>
+      <Text>Sign Up</Text>
+      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Sign Up" onPress={handleSignUp} />
+    </View>
+  );
+};
 
 export default SignUpScreen;
