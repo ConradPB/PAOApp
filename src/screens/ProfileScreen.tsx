f3c9e8139a117fceb34b74a77e5116a99f70c8dc
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 16:58:46 +0300

    updated profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const ProfileScreen = () => (
-  <View>
-    <Text>Profile Screen</Text>
-  </View>
-);
+const ProfileScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default ProfileScreen;
