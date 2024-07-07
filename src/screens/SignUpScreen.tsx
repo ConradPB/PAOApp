13db32f06c97628b3abf11d23645e58490df2155
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 23:54:25 +0300

    updated

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,42 +1,24 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RouteProp } from '@react-navigation/native';
+import { View, Text, Button } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 
 type RootStackParamList = {
   Welcome: undefined;
   SignUp: undefined;
   Login: undefined;
+  // other screens...
 };
 
-type SignUpScreenNavigationProp = StackNavigationProp<RootStackParamList, 'SignUp'>;
-type SignUpScreenRouteProp = RouteProp<RootStackParamList, 'SignUp'>;
-
-type Props = {
-  navigation: SignUpScreenNavigationProp;
-  route: SignUpScreenRouteProp;
-};
+type Props = StackScreenProps<RootStackParamList, 'SignUp'>;
 
 const SignUpScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
-      <Button title="Back to Welcome" onPress={() => navigation.navigate('Welcome')} />
+    <View>
+      <Text>Sign Up</Text>
+      <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
 
 export default SignUpScreen;
