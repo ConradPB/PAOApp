9cd0b0c0233734613f59cc6f54d07cb1ffabc7ff
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 17:05:47 +0300

    updated tasks

--- src/screens/TaskDetailsScreen.tsx
+++ src/screens/TaskDetailsScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const TaskDetailsScreen = () => (
-  <View>
-    <Text>Task Details Screen</Text>
-  </View>
-);
+const TaskListScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Task List</Text>
+    </View>
+  );
+};
 
-export default TaskDetailsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
+
+export default TaskListScreen;
