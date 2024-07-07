f307efa45145fb2d5eab190ff574e9a2596a4b5a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 17:43:42 +0300

    updated AddEditTask

--- src/screens/AddEditTaskScreen.tsx
+++ src/screens/AddEditTaskScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const AddEditTaskScreen = () => (
-  <View>
-    <Text>Add/Edit Task Screen</Text>
-  </View>
-);
+const AddEditTaskScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Add/Edit Task</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default AddEditTaskScreen;
