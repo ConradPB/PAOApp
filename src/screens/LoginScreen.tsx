725363a9a481611118e499e122ee91a28244ae35
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 16:53:54 +0300

    updated Login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,30 +1,42 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../types';
+import { RouteProp } from '@react-navigation/native';
+
+type RootStackParamList = {
+  Welcome: undefined;
+  SignUp: undefined;
+  Login: undefined;
+};
 
 type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+type LoginScreenRouteProp = RouteProp<RootStackParamList, 'Login'>;
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation<LoginScreenNavigationProp>();
+type Props = {
+  navigation: LoginScreenNavigationProp;
+  route: LoginScreenRouteProp;
+};
 
-  const handleLogin = () => {
-    // Handle login logic here
-    navigation.navigate('Home');
-  };
-
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View>
-      <Text>Login</Text>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
-      <Button title="Login" onPress={handleLogin} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <Button title="Back to Welcome" onPress={() => navigation.navigate('Welcome')} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default LoginScreen;
