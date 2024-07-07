36650ee8e04d64dcc9ea57e538bf2a12e18f354d
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 17:10:55 +0300

    updated statistics

--- src/screens/StatisticsScreen.tsx
+++ src/screens/StatisticsScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const StatisticsScreen = () => (
-  <View>
-    <Text>Statistics Screen</Text>
-  </View>
-);
+const StatisticsScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Statistics</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default StatisticsScreen;
