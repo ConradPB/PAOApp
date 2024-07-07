a65411b87a21e89f47b0c442cd0506a3ba22b9ee
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 16:51:44 +0300

    updated WelcomeScreen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,24 +1,43 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../types';
+import { RouteProp } from '@react-navigation/native';
+
+type RootStackParamList = {
+  Welcome: undefined;
+  SignUp: undefined;
+  Login: undefined;
+};
 
 type WelcomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Welcome'>;
+type WelcomeScreenRouteProp = RouteProp<RootStackParamList, 'Welcome'>;
 
-const WelcomeScreen = () => {
-  const navigation = useNavigation<WelcomeScreenNavigationProp>();
+type Props = {
+  navigation: WelcomeScreenNavigationProp;
+  route: WelcomeScreenRouteProp;
+};
 
+const WelcomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View>
-      <Text>Welcome to PAO - Break Bread</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to PÃO</Text>
       <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
       <Button title="Login" onPress={() => navigation.navigate('Login')} />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
+
 export default WelcomeScreen;
-
-
