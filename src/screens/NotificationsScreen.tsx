ec2840938e0b9f9bf46abc81c8bd937810c39894
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-07 17:03:49 +0300

    updated notifications

--- src/screens/NotificationsScreen.tsx
+++ src/screens/NotificationsScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const NotificationsScreen = () => (
-  <View>
-    <Text>Notifications Screen</Text>
-  </View>
-);
+const NotificationsScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Notifications</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
 
 export default NotificationsScreen;
