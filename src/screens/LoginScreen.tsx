f9f96bfb12904b576fc08a104a6134e1fa82bf0e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-08 06:46:50 +0300

    updated login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,19 +1,63 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
-
 
 type Props = StackScreenProps<RootStackParamList, 'Login'>;
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+
+    console.log('Logging in:', { email, password });
+    navigation.navigate('Home');
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-      <Button title="Go to Sign Up" onPress={() => navigation.navigate('SignUp')} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 24,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
+
 export default LoginScreen;
+
+
